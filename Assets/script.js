6520b22293c214b75d96f7594eaf0e3519d33cb1
Jared Rose <lfj143@gmail.com>
2021-06-25 09:16:57 -0700

    updated submission

--- Assets/script.js
+++ Assets/script.js
@@ -1,28 +1,32 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var num = "0123456789!@#$%^&*()_+-={}[]:;><";
+var num = "0123456789";
 var spcl = "!@#$%^&*()_+-={}[]:;><"
 var alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var passwordlength = document.getElementById("length").value;
 var incSpecials = document.getElementById("symbols");
 var incNum = document.getElementById("numbers");
 
+// Add event listener to generate button
+generateBtn.addEventListener("click", () => {
+var characters = alpha;
+incNum.checked ? (characters += numbers): "";
+incSpecials.checked ? (characters += symbols): "";
+password.value = genPW(length.value, characters);
+// console.log(genPW)
+});
+
 // Write password to the #password input
 function genPW(passwordlength, characters) {
   var password = "";
   
-  for (var i=0; i<passwordlength; i++){
-    Math.floor(Math.random() * characters.length);
+  for (var i=0; i<length; i++){
+    password =+ characters.charAt(
+    Math.floor(Math.random() * characters.length)
+    );
   }
   return password
   console.log(password)
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", genPW({
-// var characters = alpha;
-incNum.checked ? (characters += numbers): "";
-incSpecials.checked ? (characters += symbols): "";
-password.value = genPW(length.value, characters);
-});
